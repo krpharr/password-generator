ce03ad439845295726d0c12e124a0630b9576c84
rpharr <rpharr@richmond.edu>
2019-11-18 01:29:32 -0500

    functional password gen code

--- assets/js/passwordGeneraotor.js
+++ assets/js/passwordGeneraotor.js
@@ -1,22 +1,80 @@
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "`~!@#$%^&*()_+-{}[];':,.?";
+
+var charWeightsArray = [100, 0, 0, 0];
+var password = "";
+
+var passwordDisplay = document.getElementById("passwordDisplayID");
 var passwordLength = document.getElementById("passwordSliderID");
 var lowercaseCheck = document.getElementById("lowercaseCheckID");
 var lowercaseWeight = document.getElementById("lowercaseSliderID");
+var uppercaseCheck = document.getElementById("uppercaseCheckID");
 var uppercaseWeight = document.getElementById("uppercaseSliderID");
+var numbersCheck = document.getElementById("numbersCheckID");
 var numbersWeight = document.getElementById("numbersSliderID");
+var specialCheck = document.getElementById("specialCheckID");
 var specialWeight = document.getElementById("specialSliderID");
 
-var passwordDisplay = document.getElementById("passwordDisplayID");
 
-
-passwordDisplay.innerHTML = passwordLength.value;
-
-
-passwordLength.oninput = function() {
-    passwordDisplay.innerHTML = this.value;
+function log() {
+    console.log("********************");
+    console.log("Password\t " + password);
+    console.log("Length\t " + passwordLength.value);
+    console.log("Lower\t " + lowercaseWeight.value);
+    console.log("Upper\t " + uppercaseWeight.value);
+    console.log("Numbers\t " + numbersWeight.value);
+    console.log("Special\t " + specialWeight.value);
 }
 
-var password = "";
-var charWeightsArray = [0, 0, 0, 0];
+function init() {
+    lowercaseCheck.checked = "checked";
+    lowercaseWeight.value = "100";
+    passwordLength.value = "32";
+    generatePassword();
+}
+
+passwordLength.oninput = function() {
+    generatePassword();
+}
+
+function generatePassword() {
+    password = "";
+    var charWeightsArray = [parseInt(lowercaseWeight.value), parseInt(uppercaseWeight.value),
+        parseInt(numbersWeight.value), parseInt(specialWeight.value)
+    ];
+    console.log(charWeightsArray);
+
+    for (var i = 0; i < passwordLength.value; i++) {
+        var s = "";
+        switch (chooseWithChance(charWeightsArray)) {
+            case 0:
+                s = lower;
+                break;
+            case 1:
+                s = upper;
+                break;
+            case 2:
+                s = numbers;
+                break;
+            case 3:
+                s = special;
+                break;
+        }
+
+        console.log(s);
+
+        var c = s.charAt(Math.floor(Math.random() * s.length));
+        console.log("Random Char: " + c);
+        password = password.concat(c);
+
+    }
+    passwordDisplay.innerHTML = password;
+    console.log(password);
+
+}
+
 
 function CheckOnClick(checkID, sliderID) {
     var checkBox = document.getElementById(checkID);
@@ -26,6 +84,8 @@ function CheckOnClick(checkID, sliderID) {
     } else {
         slider.value = 0;
     }
+    generatePassword();
+    log();
 }
 
 function SliderOnClick(sliderID, checkID) {
@@ -36,6 +96,8 @@ function SliderOnClick(sliderID, checkID) {
     } else {
         checkBox.checked = true;
     }
+    generatePassword();
+    log();
 }
 
 function chooseWithChance(args) {
@@ -45,14 +107,25 @@ function chooseWithChance(args) {
     var argCount = args.length;
     var sumOfChances = 0;
     for (var i = 0; i < argCount; i++) {
-        sumOfChances += args[i];
+        sumOfChances = sumOfChances + args[i];
     }
+
+    console.log("argCount\t " + argCount);
+    console.log("sumOfChances\t " + sumOfChances);
+
+
     var r = Math.floor(Math.random() * sumOfChances);
+    console.log("r\t " + r);
+    console.log("minus args[argCount - 1]\t " + args[argCount - 1])
     r = r - args[argCount - 1];
     while (r > 0) {
         argCount--;
         sumOfChances = sumOfChances - args[argCount - 1];
         r = r - args[argCount - 1];
+        console.log(argCount, sumOfChances, r);
     }
+    console.log(argCount - 1);
     return argCount - 1;
 }
+
+init();

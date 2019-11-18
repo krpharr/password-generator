d82f8f589c66b078b275201395b569007f60c031
rpharr <rpharr@richmond.edu>
2019-11-17 22:31:51 -0500

    paired checkboxes and sliders with javascript

--- assets/js/passwordGeneraotor.js
+++ assets/js/passwordGeneraotor.js
@@ -1,4 +1,10 @@
 var passwordLength = document.getElementById("passwordSliderID");
+var lowercaseCheck = document.getElementById("lowercaseCheckID");
+var lowercaseWeight = document.getElementById("lowercaseSliderID");
+var uppercaseWeight = document.getElementById("uppercaseSliderID");
+var numbersWeight = document.getElementById("numbersSliderID");
+var specialWeight = document.getElementById("specialSliderID");
+
 var passwordDisplay = document.getElementById("passwordDisplayID");
 
 
@@ -7,4 +13,46 @@ passwordDisplay.innerHTML = passwordLength.value;
 
 passwordLength.oninput = function() {
     passwordDisplay.innerHTML = this.value;
+}
+
+var password = "";
+var charWeightsArray = [0, 0, 0, 0];
+
+function CheckOnClick(checkID, sliderID) {
+    var checkBox = document.getElementById(checkID);
+    var slider = document.getElementById(sliderID);
+    if (checkBox.checked === true) {
+        slider.value = 100;
+    } else {
+        slider.value = 0;
+    }
+}
+
+function SliderOnClick(sliderID, checkID) {
+    var slider = document.getElementById(sliderID);
+    var checkBox = document.getElementById(checkID);
+    if (slider.value == 0) {
+        checkBox.checked = false;
+    } else {
+        checkBox.checked = true;
+    }
+}
+
+function chooseWithChance(args) {
+    // takes in array of percentages and returns an index 
+    // based on weight of those percentages
+
+    var argCount = args.length;
+    var sumOfChances = 0;
+    for (var i = 0; i < argCount; i++) {
+        sumOfChances += args[i];
+    }
+    var r = Math.floor(Math.random() * sumOfChances);
+    r = r - args[argCount - 1];
+    while (r > 0) {
+        argCount--;
+        sumOfChances = sumOfChances - args[argCount - 1];
+        r = r - args[argCount - 1];
+    }
+    return argCount - 1;
 }
